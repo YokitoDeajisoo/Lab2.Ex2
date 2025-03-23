978d4a20ce65cd78ebe2fbe9072be4d09bff124f
OleksiiHoncharck <mirikixatara280@outlook.com>
2025-03-23 17:34:17 +0200

    Complete

---
+++ Lab2.Ex2.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main() {
+    double a = -50.0;
+    double b = 4.0;
+    double c = 9.0;
+    double y;
+
+    // Обчислення значення функції y
+    y = (c * a) / (a + b) + (b * c) / (c + a);
+
+    // Виведення результату
+    printf("Function value y: %.2lf\n", y);
+
+    return 0;
+}
